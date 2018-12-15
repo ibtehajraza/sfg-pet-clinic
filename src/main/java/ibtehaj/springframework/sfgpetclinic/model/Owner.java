9445f08bcbdf9abff1523def29cb182ae6a3fcb9
Ibtehaj Raza <ibtahajraza@gmail.com>
2018-12-15 18:05:40 +0500

    Added POJOs. Closes #2.

---
+++ src/main/java/ibtehaj/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package ibtehaj.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/ibtehaj/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package ibtehaj.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
