855bc602f4ca1973fe44e662b71eed5cd3dcfe18
Ibtehaj Raza <ibtehajraza@gmail.com>
2018-12-18 10:41:57 +0500

    refactoring to multimodule maven project And Added interface services. closes #1.

---
+++ pet-clinic-data/src/main/java/ibtehaj/springframework/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package ibtehaj.springframework.sfgpetclinic.services;
+
+import ibtehaj.springframework.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/ibtehaj/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package ibtehaj.springframework.sfgpetclinic.services;
+
+import ibtehaj.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
