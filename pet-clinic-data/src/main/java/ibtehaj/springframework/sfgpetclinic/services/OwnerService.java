f2ec6856704772af55172a65b9949c4f6f8701e7
Ibtehaj Raza <ibtehajraza@gmail.com>
2018-12-19 13:19:16 +0500

    Implement Map Based Services. closes #6

--- pet-clinic-data/src/main/java/ibtehaj/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/ibtehaj/springframework/sfgpetclinic/services/OwnerService.java
@@ -6,9 +6,4 @@ public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
-/*    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();*/
 }
--- pet-clinic-data/src/main/java/ibtehaj/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/ibtehaj/springframework/sfgpetclinic/services/PetService.java
@@ -5,9 +5,5 @@ import ibtehaj.springframework.sfgpetclinic.model.Pet;
 
 public interface PetService extends CrudService<Pet, Long> {
 
-/*    Pet findById(Long id);
 
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();*/
 }
