e2bf1477370a55ab8de5996b161d846d65102b6a
Ibtehaj Raza <ibtahajraza@gmail.com>
2018-12-23 13:53:05 +0500

    Added Listing of Vets on Vet's Index Page. closes #14.

--- pet-clinic-web/src/main/java/ibtehaj/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ibtehaj/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,15 +1,26 @@
 package ibtehaj.springframework.sfgpetclinic.controllers;
 
+import ibtehaj.springframework.sfgpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets","/vets/list","/vets/index","/vets/index.html"})
+    private final VetService vetService;
 
-    public String vetIndex(){
-        return "vet/vets_index";
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/", "", "/list", "/index", "/index.html"})
+    public String vetIndex(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+        return "vet/index";
     }
 
 }
